00a22a67b4d776277ab6adfae5d8441461562ada
sudo-Tiz <mathis.worksome@simplelogin.fr>
2026-09-15 09:20:36 +0200

    map: add Telescope keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,11 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map({ "n", "t" }, "<leader>S", ":%s//g<Left><Left>")
+map({ "n", "t" }, "<leader>S", ":%s//g<Left><Left>", { desc = "sed" })
+map("n", "<leader>wh", ":Telescope keymaps<CR>")
 
-vim.keymap.set("n", "<leader>cc", function()
-  vim.b.cmp_enabled = not vim.b.cmp_enabled
-  require('cmp').setup.buffer { enabled = vim.b.cmp_enabled }
-end, { desc = "Toggle cmp" })   
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
