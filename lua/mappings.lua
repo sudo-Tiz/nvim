43930cd628e9b66c1607ea841f6160d5749eff5c
sudo-Tiz <mathis.worksome@simplelogin.fr>
2026-09-16 22:02:22 +0200

    fix ESC swallow by zsh in nvim terminal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,11 +1,9 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map({ "n", "t" }, "<leader>S", ":%s//g<Left><Left>", { desc = "sed" })
 map("n", "<leader>wh", ":Telescope keymaps<CR>")
-
+map("t", "<Esc>", "<C-\\><C-n>", { noremap = true, desc = "true Esc in nvim terminal"})
